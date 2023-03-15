8ea9518ca7084ee908dc8e4e6d205e96ca50cb15
MMahmud <shametlab@gmai.com>
2023-03-15 01:43:21 -0700

    committed

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -27,7 +27,7 @@ int main(void)
 {
     char **tab;
 
-    tab = strtoq ("      ALX School         #cisfun      ");
+    tab = strtow("      ALX School         #cisfun      ");
     if (tab == NULL)
     {
         printf("Failed\n");
@@ -36,3 +36,4 @@ int main(void)
     print_tab(tab);
     return (0);
 }
+
