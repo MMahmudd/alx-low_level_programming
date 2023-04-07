e4afc497bc8695e9d58aa6c8944adc2ce4a8bcc6
MMahmudd <shametlab@gmai.com>
2023-04-07 10:29:01 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,23 +7,21 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bit_index;
+	int i, count = 0;
+	unsigned long int current;
 
-	num_of_ones = 0;
-	unsigned long int current_bit;
-
-	for (bit_index = 63; bit_index >= 0; bit_index--)
+	for (i = 63; i >= 0; i--)
 	{
-		current_bit = n >> bit_index;
+		current = n >> i;
 
-		if (current_bit & 1)
+		if (current & 1)
 		{
 			_putchar('1');
-			num_of_ones++;
+			count++;
 		}
-		else if (num_of_ones)
+		else if (count)
 			_putchar('0');
-		}
-	if (!num_of_ones)
+	}
+	if (!count)
 		_putchar('0');
 }
