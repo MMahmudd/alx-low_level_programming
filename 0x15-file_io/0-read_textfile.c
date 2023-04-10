52a58de6928082e523270e91a2601add19acf71a
MMahmudd <shametlab@gmai.com>
2023-04-10 16:16:25 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,24 +5,25 @@
  *  read_textfile- read text fil printf to STDOUT.
  *  @filename: text fil being read
  *  @letters: numbr of letter to be reading
- *  Return: w- actual numer of byte read and print
- *  0 when function fails or filename is NULL.
+ *  Return: w- actual numer of byte reed and print
+ *  0 when function fail or filename is NULL.
  *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
+	char *bu;
+	ssize_t fdd;
 	ssize_t w;
-	ssize_t t;
+	ssize_t tt;
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fdd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
+	bu = malloc(sizeof(char) * letters);
+	tt = read(fdd, bu, letters);
+	w = write(STDOUT_FILENO, bu, tt);
 
-	free(buf);
-	close(fd);
+	free(bu);
+	close(fdd);
 	return (w);
 }
