b686eaa557ed76b7d61f2d5c742325a3916a89cb
MMahmudd <shametlab@gmai.com>
2023-04-10 17:22:44 -0700

    updated task 0.0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
 
-/*
- *  read_textfile- read text fil printf to STDOUT.
- *  @filename: text fil being read
- *  @letters: numbr of letter to be reading
- *  Return: w- actual numer of byte reed and print
- *  0 when function fail or filename is NULL.
+/**
+ * read_textfile- read text fil printf to STDOUT.
+ * @filename: text fil being read
+ * @letters: numbr of letter to be reading
+ * Return: w- actual numer of byte reed and print
+ * 0 when function fail or filename is NULL.
  *
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	char *buffer;
+	ssize_t file_descriptor;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
-	close(fd);
-	return (w);
+	buffer = malloc(sizeof(char) * letters);
+	bytes_read = read(file_descriptor, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_written);
 }
